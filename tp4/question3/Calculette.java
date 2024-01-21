3f26332cdebcbb19f8cda69c791ba1556169abea
Ali Zaarour <zaarour.a@outlook.com>
2024-01-22 00:00:28 +0200

    feat:q3-impl-et-arch

---
+++ tp4/question3/Calculette.java
@@ -0,0 +1,23 @@
+package question3;
+
+import question3.tp3.PileI;
+
+/**
+ * Description of class Calculette
+ *
+ * @author Ali.z
+ * @version 21-01-2024
+ */
+public class Calculette {
+
+    private PileI<Integer> pile;
+
+    public Calculette(PileI<Integer> pile) {
+        this.pile = pile;
+    }
+    //public void push(int value) throws PilePleineException 
+    //public int pop() throws PileVideException
+    //public int peek() throws PileVideException 
+    //public void performOperation(Operation op) throws PileVideException, PilePleineException 
+    // Additional methods as needed...
+}
--- tp4/question3/IHMCalculette.java
+++ tp4/question3/IHMCalculette.java
@@ -7,15 +7,34 @@ import javax.swing.*;
 import java.util.Observer;
 import java.util.Observable;
 
+/**
+ * 1.Chaque partie a son travail : Dans notre programme, il y a trois grandes parties :
+ * le modèle (qui garde les données), la vue (qui montre les choses à l'utilisateur),
+ * et le contrôleur (qui fait le lien entre le modèle et la vue). Vue2 est juste une
+ * nouvelle partie qui montre les informations. Elle ne change pas le travail des autres
+ * parties.
+ * 2.Observer : Vue2 regarde le modèle. Quand le modèle change (par exemple, si nous
+ * ajoutons ou enlevons un nombre), il informe Vue2. Ainsi, Vue2 sait qu'il doit mettre
+ * à jour ce qu'il montre. Cela se fait automatiquement et n'a pas besoin de changer le
+ * modèle.
+ * 3.Vues séparées : Vue2 est indépendant. Cela signifie que son ajout ne dérange pas
+ * les autres vues. Chaque vue s'occupe de ses propres affaires.
+ * 4.Petits changements : Pour ajouter Vue2, nous modifions seulement la classe 
+ * IHMCalculette. C'est comme ajouter un nouveau tableau dans une pièce sans changer
+ * les autres meubles.
+*/
+
 public class IHMCalculette extends JFrame {
     public IHMCalculette() {
         super("IHM Calculette");
         PileModele<Integer> modele = new PileModele<Integer>(new Pile2<Integer>(5));
         Controleur controle = new Controleur(modele);
         Vue vue = new Vue(modele);
+        Vue2 vue2 = new Vue2(modele);
 
         setLayout(new GridLayout(2, 1));
         add(vue);
+        add(vue2);
         add(controle);
         pack();
         setLocation(200,200);
@@ -27,3 +46,5 @@ public class IHMCalculette extends JFrame {
         new IHMCalculette();
     }
 }
+
+
